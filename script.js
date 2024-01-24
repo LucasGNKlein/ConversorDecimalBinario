6afedb0f7dd03cbe64ff95cadc309901a58e18fa
Lucas (Sekth) Klein <86809331+LucasGNKlein@users.noreply.github.com>
2024-01-24 10:05:58 -0300

    COMMENTS

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
+/* FUNÇÃO QUE CONVERTE DE BINÁRIO PARA DECIMAL*/
 function convertBinaryToDecimal() {
-  const binaryInput = document.getElementById('binary-input').value;
+  const binaryInput = document.getElementById('binary-input').value; /*DEFININDO VARIÁVEIS E SETANDO SAÍDAS*/
   const decimalOutput = document.getElementById('decimal-output');
 
   const decimalNumber = parseInt(binaryInput, 2);
   if (isNaN(decimalNumber)) {
-    decimalOutput.textContent = 'Entrada inválida';
+    decimalOutput.textContent = 'Entrada inválida'; /*MENSAGEM DE AVISO SE NÃO FOR DECIMAL*/
   } else {
     decimalOutput.textContent = 'Decimal: ' + decimalNumber;
   }
 }
 
+/* FUNÇÃO QUE CONVERTE DE DECIMAL PARA BINÁRIO*/
 function convertDecimalToBinary() {
   const decimalInput = document.getElementById('decimal-input').value;
   const binaryOutput = document.getElementById('binary-output');
